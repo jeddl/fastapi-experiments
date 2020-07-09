d42dfeae57b3fe5ed38cd69abf0b046418c37ad3
JeDDL <lmetaballsj@gmail.com>
2020-07-08 20:55:47 -0400

    Add Middleware

--- src/application.py
+++ src/application.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 
 
 def app_init():
     app = FastAPI()
+
+    @app.middleware("http")
+    async def add_process_time_header(request: Request, call_next):
+        print(f"Request path: {request.url.path}") # e.g. Request path: /foo
+        response = await call_next(request)
+        print(f"Response: {response}") # <starlette.responses.StreamingResponse object at 0x7f6fd0dd37c0>
+        return response
 
     @app.get("/foo")
     def foo():
